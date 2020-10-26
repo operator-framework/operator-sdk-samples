08e12152aeb1c09dd9057c79caba0f579ebb122e
Ish Shah <ishah@redhat.com>
2020-10-26 15:12:21 -0700

    Generate all Samples Script (#162)

--- .generate/gen-all.sh
+++ .generate/gen-all.sh
@@ -30,14 +30,57 @@ else
   reset=''
 fi
 
+#===================================================================
+# FUNCTION trap_add ()
+#
+# Purpose:  prepends a command to a trap
+#
+# - 1st arg:  code to add
+# - remaining args:  names of traps to modify
+#
+# Example:  trap_add 'echo "in trap DEBUG"' DEBUG
+#
+# See: http://stackoverflow.com/questions/3338030/multiple-bash-traps-for-the-same-signal
+#===================================================================
+
+function trap_add() {
+    trap_add_cmd=$1; shift || fatal "${FUNCNAME} usage error"
+    new_cmd=
+    for trap_add_name in "$@"; do
+        # Grab the currently defined trap commands for this trap
+        existing_cmd=`trap -p "${trap_add_name}" |  awk -F"'" '{print $2}'`        # Define default command
+        [ -z "${existing_cmd}" ] && existing_cmd="echo exiting @ `date`"        # Generate the new command
+        new_cmd="${trap_add_cmd};${existing_cmd}"        # Assign the test
+         trap   "${new_cmd}" "${trap_add_name}" || \
+                fatal "unable to add to trap ${trap_add_name}"
+    done
+}
 function header_text {
   echo "$header$*$reset"
 }
-
-#  NOTE: The script will use the bin installed locally
-#  Ensure that you have the version required with `operator-sdk version`
+# edit the release version before execute the script
+RELEASE_VERSION=v1.1.0
+function install_bin() {
+  header_text "installing the SDK version ${RELEASE_VERSION}"
+  TMPDIR="$(mktemp -d)"
+  trap_add 'rm -rf $TMPDIR' EXIT
+  pushd "$TMPDIR"
+  git clone https://github.com/operator-framework/operator-sdk
+  cd operator-sdk
+  git checkout ${RELEASE_VERSION}
+  git checkout -b generate-release-${RELEASE_VERSION}
+  make tidy
+  make install
+  operator-sdk version
+}
 
 ROOTDIR="$(pwd)"
+
+install_bin
+
+header_text "updating samples"
+
+cd $ROOTDIR
 cd ../go/.generate/
 ./gen-go-sample.sh
 
--- go/memcached-operator/api/v1alpha1/memcached_types.go
+++ go/memcached-operator/api/v1alpha1/memcached_types.go
@@ -28,12 +28,13 @@ type MemcachedSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	Size int32 `json:"size"`
+	
+    Size int32 `json:"size"`
 }
 
 // MemcachedStatus defines the observed state of Memcached
 type MemcachedStatus struct {
-	Nodes []string `json:"nodes"`
+    Nodes []string `json:"nodes"`
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 }
